748bd52f040e8ed7f63b266e0c27dda25e97ac9e
Essi Tepponen <43061528+essitepp@users.noreply.github.com>
2022-01-20 19:13:14 +0200

    added new test

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to pokemon page', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('weedle').click()
+    cy.contains('shield dust')
+  })
 })
